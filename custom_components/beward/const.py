6b1dad2afc69dc17295fea5b38efb29b8a78307b
Andrey Khrolenok <andrey@khrolenok.ru>
2019-08-13 21:11:27 +0300

    Version bump

--- custom_components/beward/const.py
+++ custom_components/beward/const.py
@@ -4,7 +4,7 @@ from beward.const import ALARM_MOTION, ALARM_SENSOR
 
 # Base component constants
 DOMAIN = "beward"
-VERSION = "0.5.0"
+VERSION = "1.0.0"
 DATA_BEWARD = DOMAIN
 UPDATE_BEWARD = f"{DOMAIN}_update"
 REQUIRED_FILES = [
