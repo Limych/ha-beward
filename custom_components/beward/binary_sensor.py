f47144a7fd2ac9a843dae36dadfe2a0476a71771
Andrey Khrolenok <andrey@khrolenok.ru>
2020-05-15 14:10:43 +0300

    Rebuild development environment

--- custom_components/beward/binary_sensor.py
+++ custom_components/beward/binary_sensor.py
@@ -3,17 +3,27 @@
 import logging
 from datetime import timedelta
 
-from homeassistant.components.binary_sensor import BinarySensorDevice, \
-    DEVICE_CLASS_MOTION, DEVICE_CLASS_CONNECTIVITY
-from homeassistant.const import ATTR_ATTRIBUTION, CONF_NAME, \
-    CONF_BINARY_SENSORS
+from homeassistant.components.binary_sensor import (
+    BinarySensorDevice,
+    DEVICE_CLASS_MOTION,
+    DEVICE_CLASS_CONNECTIVITY,
+)
+from homeassistant.const import ATTR_ATTRIBUTION, CONF_NAME, CONF_BINARY_SENSORS
 from homeassistant.core import callback
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
 
 import beward
-from .const import BINARY_SENSOR_SCAN_INTERVAL_SECS, EVENT_DING, \
-    EVENT_MOTION, EVENT_ONLINE, CAT_DOORBELL, CAT_CAMERA, ATTRIBUTION, \
-    ATTR_DEVICE_ID, DATA_BEWARD
+from .const import (
+    BINARY_SENSOR_SCAN_INTERVAL_SECS,
+    EVENT_DING,
+    EVENT_MOTION,
+    EVENT_ONLINE,
+    CAT_DOORBELL,
+    CAT_CAMERA,
+    ATTRIBUTION,
+    ATTR_DEVICE_ID,
+    DOMAIN,
+)
 from .helpers import service_signal
 
 _LOGGER = logging.getLogger(__name__)
@@ -22,23 +32,19 @@ SCAN_INTERVAL = timedelta(seconds=BINARY_SENSOR_SCAN_INTERVAL_SECS)
 
 # Sensor types: Name, category, class
 BINARY_SENSORS = {
-    EVENT_DING: (
-        'Ding', [CAT_DOORBELL], None),
-    EVENT_MOTION: (
-        'Motion', [CAT_DOORBELL, CAT_CAMERA], DEVICE_CLASS_MOTION),
-    EVENT_ONLINE: (
-        'Online', [CAT_DOORBELL, CAT_CAMERA], DEVICE_CLASS_CONNECTIVITY),
+    EVENT_DING: ("Ding", [CAT_DOORBELL], None),
+    EVENT_MOTION: ("Motion", [CAT_DOORBELL, CAT_CAMERA], DEVICE_CLASS_MOTION),
+    EVENT_ONLINE: ("Online", [CAT_DOORBELL, CAT_CAMERA], DEVICE_CLASS_CONNECTIVITY),
 }
 
 
-async def async_setup_platform(hass, config, async_add_entities,
-                               discovery_info=None):
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Set up a binary sensors for a Beward device."""
     if discovery_info is None:
         return
 
     name = discovery_info[CONF_NAME]
-    controller = hass.data[DATA_BEWARD][name]
+    controller = hass.data[DOMAIN][name]
     category = None
     if isinstance(controller.device, beward.BewardCamera):
         category = CAT_CAMERA
@@ -48,8 +54,7 @@ async def async_setup_platform(hass, config, async_add_entities,
     sensors = []
     for sensor_type in discovery_info[CONF_BINARY_SENSORS]:
         if category in BINARY_SENSORS.get(sensor_type)[1]:
-            sensors.append(
-                BewardBinarySensor(controller, sensor_type))
+            sensors.append(BewardBinarySensor(controller, sensor_type))
 
     async_add_entities(sensors, True)
 
@@ -64,12 +69,12 @@ class BewardBinarySensor(BinarySensorDevice):
         self._unsub_dispatcher = None
         self._sensor_type = sensor_type
         self._controller = controller
-        self._name = "{0} {1}".format(
-            self._controller.name, BINARY_SENSORS.get(self._sensor_type)[0])
-        self._device_class = BINARY_SENSORS.get(self._sensor_type)[2]
+        self._name = "{} {}".format(
+            self._controller.name, BINARY_SENSORS[self._sensor_type][0]
+        )
+        self._device_class = BINARY_SENSORS[self._sensor_type][2]
         self._state = None
-        self._unique_id = '{}-{}'.format(self._controller.unique_id,
-                                         self._sensor_type)
+        self._unique_id = f"{self._controller.unique_id}-{self._sensor_type}"
 
     @property
     def name(self):
@@ -117,21 +122,26 @@ class BewardBinarySensor(BinarySensorDevice):
     @callback
     def _update_callback(self, update_ha_state=True):
         """Get the latest data and updates the state."""
-        state = self._controller.available \
-            if self._sensor_type == EVENT_ONLINE \
+        state = (
+            self._controller.available
+            if self._sensor_type == EVENT_ONLINE
             else self._controller.event_state.get(self._sensor_type, False)
+        )
         if self._state != state:
             self._state = state
-            _LOGGER.debug('%s binary sensor state changed to "%s"', self._name,
-                          self._state)
+            _LOGGER.debug(
+                '%s binary sensor state changed to "%s"', self._name, self._state
+            )
             if update_ha_state:
                 self.async_schedule_update_ha_state()
 
     async def async_added_to_hass(self):
         """Register callbacks."""
         self._unsub_dispatcher = async_dispatcher_connect(
-            self.hass, service_signal('update', self._controller.unique_id),
-            self._update_callback)
+            self.hass,
+            service_signal("update", self._controller.unique_id),
+            self._update_callback,
+        )
 
     async def async_will_remove_from_hass(self):
         """Disconnect from update signal."""
--- custom_components/beward/const.py
+++ custom_components/beward/const.py
@@ -4,31 +4,30 @@ from beward.const import ALARM_MOTION, ALARM_SENSOR
 
 # Base component constants
 DOMAIN = "beward"
-VERSION = "1.0.9"
+VERSION = "1.1.1"
 ISSUE_URL = "https://github.com/Limych/ha-beward/issues"
-DATA_BEWARD = DOMAIN
 ATTRIBUTION = "Data provided by Beward device."
 
 SUPPORT_LIB_URL = "https://github.com/Limych/py-beward/issues/new/choose"
 
-CONF_EVENTS = 'events'
-CONF_RTSP_PORT = 'rtsp_port'
-CONF_STREAM = 'stream'
-CONF_FFMPEG_ARGUMENTS = 'ffmpeg_arguments'
-CONF_CAMERAS = 'cameras'
+CONF_EVENTS = "events"
+CONF_RTSP_PORT = "rtsp_port"
+CONF_STREAM = "stream"
+CONF_FFMPEG_ARGUMENTS = "ffmpeg_arguments"
+CONF_CAMERAS = "cameras"
 
-EVENT_ONLINE = 'online'
-EVENT_MOTION = 'motion'
-EVENT_DING = 'ding'
+EVENT_ONLINE = "online"
+EVENT_MOTION = "motion"
+EVENT_DING = "ding"
 
 ALARMS_TO_EVENTS = {
     ALARM_MOTION: EVENT_MOTION,
     ALARM_SENSOR: EVENT_DING,
 }
 
-ATTR_DEVICE_ID = 'device_id'
+ATTR_DEVICE_ID = "device_id"
 
-CAT_DOORBELL = 'doorbell'
-CAT_CAMERA = 'camera'
+CAT_DOORBELL = "doorbell"
+CAT_CAMERA = "camera"
 
 BINARY_SENSOR_SCAN_INTERVAL_SECS = 5
